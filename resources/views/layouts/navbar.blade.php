821efcf27a09430ebb01f6d389a7acf5ae1a87a0
ulumarul <112668758+ulumarul@users.noreply.github.com>
2024-05-29 21:01:30 +0700

    Navbar small fix

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -3,7 +3,7 @@
         <a href="#" class="navbar-brand">
             <img src="{{ asset('assets/icongs.png') }}" alt="Logo" style="width: 60px;">
         </a>
-        <ul class="navbar-nav ms-auto">
+        <ul class="navbar-nav ms-auto d-flex flex-row gap-3">
             <li class="nav-item">
                 <a href="#" class="nav-link fw-bold">Login</a>
             </li>
