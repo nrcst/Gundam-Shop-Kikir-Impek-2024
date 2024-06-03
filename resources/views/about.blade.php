8424ee069872ccb393a16cbbcc5e5899eb60ce98
ulumarul <112668758+ulumarul@users.noreply.github.com>
2024-06-03 22:43:33 +0700

    About page addition, Footer minor fixes

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,8 +2,16 @@
 @section("title", "ABOUT")
 @section("content")
 
-<div style="height: 600px;" class="container-fluid">
+<div class="vstack gap-3 mt-3 mb-3">
+    <div style="height: 250px; background-color: #e0ceb7;" class="container-fluid">
 
+    </div>
+    <div style="height: 250px; background-color: #b29f8e;" class="container-fluid">
+
+    </div>
+    <div style="height: 250px; background-color: #e0ceb7;" class="container-fluid">
+
+    </div>
 </div>
 
 @endsection
--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,10 +1,10 @@
 </main>
-<div style="background-color: #c7b7a3;" class="py-5">
+<div style="background-color: #c7b7a3; height: 100px;" class="container-fluid">
     <div class="container my-2">
     </div>
 </div>
     <footer style="background-color: #fff8e3; font-family: 'sans-serif'; color: #5f402b;" class="text-center p-1 fw-bold">
-        &#169; GUNDAM SPOT X
+        © GUNDAM SPOT X
     </footer>
     <script src="{{asset('js/bootstrap.bundle.min.js')}}"></script>
     <script src="{{asset('js/jquery.js')}}"></script>
