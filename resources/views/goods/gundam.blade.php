ed658f82cdde93c53d0d5d69e8ccc745f5442d68
nrcst <155081275+nrcst@users.noreply.github.com>
2024-06-04 19:28:33 +0700

    feat: Add search bar to goods page

--- resources/views/goods/gundam.blade.php
+++ resources/views/goods/gundam.blade.php
@@ -1,7 +1,11 @@
 @extends("goods_layouts.layout")
+
 @section("title", "Gundam | Gundam Spot")
 @section("content")
 
-<div class="search-bar"></div>
+<div class="search-bar">
+    <input type="text" placeholder="Search...">
+    <button type="submit">Search</button>
+</div>
 
 @endsection
